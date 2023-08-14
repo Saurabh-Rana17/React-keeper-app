930708f3c9be86ab05d79e7eac1bb50ab4519eb1
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-15 00:43:23 +0530

    rendering header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 function Header() {
-  ReactDOM.render(
-    <div>
-      <header>
-        <h1>Keeper</h1>
-      </header>
-    </div>,
-
-    document.getElementById("head")
+  return (
+    <header>
+      <h1>Keeper</h1>
+    </header>
   );
 }
 export default Header;
