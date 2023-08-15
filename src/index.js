87672680116ae561197b0305cb0c7fa1761cec80
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-08-15 13:59:59 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -14,15 +14,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-ReactDOM.render(
-  <div>
-    {" "}
-    <Header />
-    <Note />
-    <Footer />
-  </div>,
-  document.getElementById("root")
-);
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
